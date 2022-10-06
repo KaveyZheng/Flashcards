f65c1cf6641cf240c62176789bc773bcb1bc8113
KaveyZheng <kaveyzheng1@gmail.com>
2022-10-05 23:05:32 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Kavey Zheng on 10/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
